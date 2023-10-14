2c3a731d712999f76019faaa8beb2efce4b97421
jdsvera <142670438+judgenavera@users.noreply.github.com>
2023-10-13 23:27:58 -0700

    commit

---
+++ variables.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navera;
+
+/**
+ *
+ * @author Student01
+ */
+public class Navera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int number;
+       number=69;
+       char letter;
+       letter = 'A';
+       boolean result;
+       result=true;
+       String str;
+       str= "Hello World";
+       
+       System.out.println("number = " + number);
+       System.out.println("letter = " + letter);
+       System.out.println("result = " + result);
+       System.out.println("Str = " + str);
+    }
+}
